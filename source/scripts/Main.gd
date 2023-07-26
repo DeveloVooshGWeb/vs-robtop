fc642aafb92c0f4ac3219eb55e9e8e7f64c40634
GrowtopiaFli <63938719+GrowtopiaFli@users.noreply.github.com>
2023-07-26 15:01:26 +0800

    a

--- source/scripts/Main.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-# var mainScene:String = "charter/ChartingState";
-# var mainScene:String = "scenes/PlayState";
-var mainScene:String = "charter/ChartingState";
-
-func _ready():
-	SceneTransition.switchAbsolute(mainScene);
-	pass;
